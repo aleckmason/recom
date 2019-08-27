bfca5aef1b8777c61cb7635e6d411e944edd1172
aleckmason <aleckmason@163.com>
2019-08-27 19:03:15 +0800

    过滤自身

--- app/src/main/java/com/szc/recommend/DetailActivity.java
+++ app/src/main/java/com/szc/recommend/DetailActivity.java
@@ -5,7 +5,9 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 import recommend.aleck.com.recommend.R;
@@ -25,7 +27,29 @@ public class DetailActivity extends Activity {
 
         WebView mWebView = findViewById(R.id.weibview);
         String url = getIntent().getStringExtra("url");
+
+
+        WebSettings settings = mWebView.getSettings();
+        settings.setJavaScriptCanOpenWindowsAutomatically(true);//设置js可以直接打开窗口，如window.open()，默认为false
+        settings.setJavaScriptEnabled(true);//是否允许执行js，默认为false。设置true时，会提醒可能造成XSS漏洞
+        settings.setSupportZoom(true);//是否可以缩放，默认true
+        settings.setBuiltInZoomControls(true);//是否显示缩放按钮，默认false
+        settings.setUseWideViewPort(true);//设置此属性，可任意比例缩放。大视图模式
+        settings.setLoadWithOverviewMode(true);//和setUseWideViewPort(true)一起解决网页自适应问题
+        settings.setAppCacheEnabled(true);//是否使用缓存
+        settings.setDomStorageEnabled(true);//DOM Storage
+
         mWebView.loadUrl(url);
+
+        //该方法解决的问题是打开浏览器不调用系统浏览器，直接用webview打开
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
     }
 
 
