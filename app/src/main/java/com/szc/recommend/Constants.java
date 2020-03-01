163a02aaa122375cc1cca452161656823a3ccb94
aleckmason <aleckmason@163.com>
2020-03-01 11:05:37 +0800

    更新manifest

--- app/src/main/java/com/szc/recommend/Constants.java
+++ app/src/main/java/com/szc/recommend/Constants.java
@@ -8,6 +8,6 @@ public class Constants {
     //家里
     //public final static String   ROOT_URL = "http://192.168.0.7:9090/appsystem/";
     //正式
-    public final static String ROOT_URL = "http://www.moningcall.cn/appsystem/";
+    public final static String ROOT_URL = "https://www.moningcall.cn/appsystem/";
     public static final String GET_APP_LIST = ROOT_URL + "main/getAppList";
 }
